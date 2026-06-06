6abb34fee6f122a2fae8cfbab00e4ea8092a82d0
Jil patel <jilpatel1094@gmail.com>
2026-06-06 17:02:46 +0530

    Create RepeatedNum.java

---
+++ pattern/RepeatedNum.java
@@ -0,0 +1,20 @@
+class obj{
+    void method(){
+    int num=4;
+    // int n=1;
+     for (int i = 0; i <num; i++) {
+        // for space
+        for (int j = 0; j <i+1; j++) {
+            System.out.print(i+1);
+            
+        }
+         System.out.println();
+     }
+    }
+}
+public class p1{
+    public static void main(String[] args) {
+    obj ob=new obj();
+    ob.method();
+    }
+}
