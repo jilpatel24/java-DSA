3d3285542437babaccc1a6171879910fe44a0f37
Jil patel <jilpatel1094@gmail.com>
2026-06-12 17:05:36 +0530

    Create LinearSearch.java

---
+++ Array/LinearSearch.java
@@ -0,0 +1,22 @@
+class array{
+    
+    int arr[]={1,23,3,14,5,16,7,8}; 
+    boolean isThere = false;
+    void findNum(){
+    
+    int target=16;
+    System.out.println("Target element is : "+target);
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i] == target){
+        System.out.println("Index : "+i);
+        }
+    }
+}
+}
+public class p1{
+     public static void main(String[] args) {
+    array obj=new array();
+    obj.findNum();
+}
+}
+  
