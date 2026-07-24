216eeb12c2799af6a24286780c81b4eda95e73d8
Jil patel <jilpatel1094@gmail.com>
2026-07-24 07:59:38 +0530

    Create fibonacci.java

---
+++ Recursion_problems/fibonacci.java
@@ -0,0 +1,17 @@
+class Solution {
+ static int fib(int n){
+    if(n == 0){
+      return 0;
+    }
+    if(n == 1){
+      return 1;
+    }else{
+  return fib(n-1)+fib(n-2);
+    }
+  }
+    public static void main(String[] args) {
+      int n = Integer.parseInt(args[0]);
+      int ans = fib(n);
+      System.out.println(ans);
+    }
+}
